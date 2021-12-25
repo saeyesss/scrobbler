4386616de148664fe5a570311b2c4c84c4ac191e
saeyesss <94211639+saeyesss@users.noreply.github.com>
2021-12-25 20:03:44 +0530

    init2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="fluid m-0 p-0">
-    <!-- <Login /> -->
     <Landing />
   </div>
 </template>
 
 <script>
-// import Login from "@/components/Login";
-// import DashboardHome from "@/components/DashboardHome";
-// import Footer from '../components/footer.vue';
 import Landing from "@/components/Landing";
 export default {
   name: "Home",
